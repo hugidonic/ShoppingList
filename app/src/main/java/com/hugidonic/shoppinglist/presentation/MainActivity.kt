e4dff214218f080eb859937fba57013b3133a2a1
Hugidonic <netvaca@ya.ru>
2023-03-15 16:36:11 +0300

    RecyclerView.Adapter -> ListAdapter

--- app/src/main/java/com/hugidonic/shoppinglist/presentation/MainActivity.kt
+++ app/src/main/java/com/hugidonic/shoppinglist/presentation/MainActivity.kt
@@ -20,7 +20,7 @@ class MainActivity : AppCompatActivity() {
 
 		viewModel = ViewModelProvider(this)[MainViewModel::class.java]
 		viewModel.shopList.observe(this) {
-			shopListAdapter.shopList = it
+			shopListAdapter.submitList(it)
 		}
 	}
 
@@ -53,7 +53,7 @@ class MainActivity : AppCompatActivity() {
 			): Boolean = false
 
 			override fun onSwiped(viewHolder: RecyclerView.ViewHolder, direction: Int) {
-				val shopItem = shopListAdapter.shopList[viewHolder.adapterPosition]
+				val shopItem = shopListAdapter.currentList[viewHolder.adapterPosition]
 				viewModel.deleteShopItem(shopItem)
 
 			}
--- app/src/main/java/com/hugidonic/shoppinglist/presentation/ShopListAdapter.kt
+++ app/src/main/java/com/hugidonic/shoppinglist/presentation/ShopListAdapter.kt
@@ -1,53 +1,36 @@
 package com.hugidonic.shoppinglist.presentation
 
 import android.view.LayoutInflater
-import android.view.View
 import android.view.ViewGroup
-import android.widget.TextView
-import androidx.recyclerview.widget.ItemTouchHelper
-import androidx.recyclerview.widget.RecyclerView
+import androidx.recyclerview.widget.ListAdapter
 import com.hugidonic.shoppinglist.R
 import com.hugidonic.shoppinglist.domain.ShopItem
 
-class ShopListAdapter(): RecyclerView.Adapter<ShopListAdapter.ShopListViewHolder>() {
+class ShopListAdapter(): ListAdapter<ShopItem, ShopItemViewHolder>(ShopItemDiffCallback()) {
 
-	var shopList = listOf<ShopItem>()
-		set(value) {
-			field = value
-			notifyDataSetChanged()
-		}
-
-//	var onShopItemClickListener: OnShopItemLongClickListener? = null
 	var onShopItemClickListener: ((shopItem: ShopItem) -> Unit)? = null
 	var onShopItemLongClickListener: ((shopItem: ShopItem) -> Unit)? = null
 
-	override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ShopListViewHolder {
+	override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ShopItemViewHolder {
 		val layoutId = when (viewType) {
 			ENABLED_VIEW_TYPE -> R.layout.item_shop_enabled
 			DISABLED_VIEW_TYPE -> R.layout.item_shop_disabled
 			else -> throw RuntimeException("Unknown view type: $viewType")
 		}
 		val view = LayoutInflater.from(parent.context).inflate(layoutId, parent, false)
-		return ShopListViewHolder(view)
+		return ShopItemViewHolder(view)
 	}
 
 	override fun getItemViewType(position: Int): Int {
-		return if (shopList[position].enabled) {
+		return if (getItem(position).enabled) {
 			ENABLED_VIEW_TYPE
 		} else {
 			DISABLED_VIEW_TYPE
 		}
 	}
 
-	private fun getItem(position: Int): ShopItem {
-		return shopList[position]
-	}
 
-	override fun getItemCount(): Int {
-		return shopList.size
-	}
-
-	override fun onBindViewHolder(holder: ShopListViewHolder, position: Int) {
+	override fun onBindViewHolder(holder: ShopItemViewHolder, position: Int) {
 		val shopItem = getItem(position)
 		holder.bind(shopItem)
 		holder.view.setOnLongClickListener {
@@ -59,20 +42,6 @@ class ShopListAdapter(): RecyclerView.Adapter<ShopListAdapter.ShopListViewHolder
 		}
 
 	}
-
-	class ShopListViewHolder(val view: View): RecyclerView.ViewHolder(view) {
-		private val tvName = view.findViewById<TextView>(R.id.tv_name)
-		private val tvCount = view.findViewById<TextView>(R.id.tv_count)
-
-		fun bind(shopItem: ShopItem) {
-			tvName.text = shopItem.name
-			tvCount.text = shopItem.count.toString()
-		}
-	}
-//
-//	interface OnShopItemLongClickListener {
-//		fun onShopItemLongClick(shopItem: ShopItem)
-//	}
 
 	companion object {
 		const val ENABLED_VIEW_TYPE = 1
