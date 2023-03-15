0a3c3b3c8ed2079352ea7147cfdb1a5362437375
Hugidonic <netvaca@ya.ru>
2023-03-15 16:10:46 +0300

    list to sorted set

--- app/src/main/java/com/hugidonic/shoppinglist/data/ShopListRepositoryImpl.kt
+++ app/src/main/java/com/hugidonic/shoppinglist/data/ShopListRepositoryImpl.kt
@@ -10,12 +10,12 @@ import kotlin.random.Random
 object ShopListRepositoryImpl: ShopListRepository {
 
     private val shopListLD = MutableLiveData<List<ShopItem>>()
-    private val shopList = mutableListOf<ShopItem>()
+    private val shopList = sortedSetOf<ShopItem>({ o1, o2 -> o1.id.compareTo(o2.id) })
     private var autoIncrementId = 0
 
 
     init {
-        for (i in 0 until 1000) {
+        for (i in 0 until 100) {
             val item = ShopItem("Name $i", i, Random.nextBoolean())
             addShopItem(item)
         }
