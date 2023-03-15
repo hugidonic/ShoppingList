134310597586572340a6ffde3ff5d1bd5b833857
Hugidonic <netvaca@ya.ru>
2023-03-15 16:35:54 +0300

    Shop item ViewHolder file class created

---
+++ app/src/main/java/com/hugidonic/shoppinglist/presentation/ShopItemViewHolder.kt
@@ -0,0 +1,17 @@
+package com.hugidonic.shoppinglist.presentation
+
+import android.view.View
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import com.hugidonic.shoppinglist.R
+import com.hugidonic.shoppinglist.domain.ShopItem
+
+class ShopItemViewHolder(val view: View): RecyclerView.ViewHolder(view) {
+	private val tvName = view.findViewById<TextView>(R.id.tv_name)
+	private val tvCount = view.findViewById<TextView>(R.id.tv_count)
+
+	fun bind(shopItem: ShopItem) {
+		tvName.text = shopItem.name
+		tvCount.text = shopItem.count.toString()
+	}
+}
