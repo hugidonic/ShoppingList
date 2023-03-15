17b87adb0902827f9b3fb7d9acd38dedced0c1ab
Hugidonic <netvaca@ya.ru>
2023-03-15 12:39:19 +0300

    Recycler view setup func

--- app/src/main/java/com/hugidonic/shoppinglist/presentation/MainActivity.kt
+++ app/src/main/java/com/hugidonic/shoppinglist/presentation/MainActivity.kt
@@ -4,26 +4,39 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import androidx.lifecycle.ViewModelProvider
+import androidx.recyclerview.widget.RecyclerView
 import com.hugidonic.shoppinglist.R
 
 class MainActivity : AppCompatActivity() {
 
-    private lateinit var viewModel: MainViewModel
+	private lateinit var viewModel: MainViewModel
+	private lateinit var shopListAdapter: ShopListAdapter
 
-    private var count = 0
+	override fun onCreate(savedInstanceState: Bundle?) {
+		super.onCreate(savedInstanceState)
+		setContentView(R.layout.activity_main)
+		setupRecyclerView()
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-        viewModel = ViewModelProvider(this)[MainViewModel::class.java]
+		viewModel = ViewModelProvider(this)[MainViewModel::class.java]
+		viewModel.shopList.observe(this) {
+			Log.d("MainActivity test", it.toString())
+			shopListAdapter.shopList = it
+		}
+	}
 
-        viewModel.shopList.observe(this) {
-            Log.d("MainActivity test", it.toString())
-            if (count == 0) {
-                count++
-                val item = it[0]
-                viewModel.changeEnableState(item)
-            }
-        }
-    }
+	private fun setupRecyclerView() {
+		val rvShopList = findViewById<RecyclerView>(R.id.rv_shop_list)
+		with(rvShopList) {
+			shopListAdapter = ShopListAdapter()
+			adapter = shopListAdapter
+			recycledViewPool.setMaxRecycledViews(
+				ShopListAdapter.ENABLED_VIEW_TYPE,
+				ShopListAdapter.maxPoolSize
+			)
+			recycledViewPool.setMaxRecycledViews(
+				ShopListAdapter.DISABLED_VIEW_TYPE,
+				ShopListAdapter.maxPoolSize
+			)
+		}
+	}
 }
