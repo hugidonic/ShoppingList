45242416cb8949c4b2374657eeab41df74019629
Hugidonic <netvaca@ya.ru>
2023-03-17 18:26:16 +0300

    ShopItemActivity design created
    (cherry picked from commit c4311ece6b2c1ef591104d3a93bd58582ad87f61)

---
+++ app/src/main/java/com/hugidonic/shoppinglist/presentation/ShopItemActivity.kt
@@ -0,0 +1,11 @@
+package com.hugidonic.shoppinglist.presentation
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ShopItemActivity : AppCompatActivity() {
+	override fun onCreate(savedInstanceState: Bundle?) {
+		super.onCreate(savedInstanceState)
+		setContentView(R.layout.activity_shop_item)
+	}
+}
