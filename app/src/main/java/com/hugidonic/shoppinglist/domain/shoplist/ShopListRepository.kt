4d91984b33a1944e6464c9656447acc6c6206340
Hugidonic <netvaca@ya.ru>
2023-03-31 22:18:05 +0300

    ShopListRepository interface implements suspend functions

--- app/src/main/java/com/hugidonic/shoppinglist/domain/shoplist/ShopListRepository.kt
+++ app/src/main/java/com/hugidonic/shoppinglist/domain/shoplist/ShopListRepository.kt
@@ -4,13 +4,13 @@ import androidx.lifecycle.LiveData
 import com.hugidonic.shoppinglist.domain.ShopItem
 
 interface ShopListRepository {
-    fun addShopItem(shopItem: ShopItem): Unit
+    suspend fun addShopItem(shopItem: ShopItem)
 
-    fun deleteShopItem(shopItem: ShopItem): Unit
+    suspend fun deleteShopItem(shopItem: ShopItem)
 
-    fun editShopItem(shopItem: ShopItem): Unit
+    suspend fun editShopItem(shopItem: ShopItem)
 
-    fun getShopItemById(shopItemId: Int): ShopItem
+    suspend fun getShopItemById(shopItemId: Int): ShopItem
 
     fun getShopList(): LiveData<List<ShopItem>>
 }
